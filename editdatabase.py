81e4786f6d055347995392a1bcca6c630a0941cd
sharg999 <sharg999@users.noreply.github.com>
2014-04-17 01:31:49 -0700

    Update editdatabase.py

--- editdatabase.py
+++ editdatabase.py
@@ -17,7 +17,7 @@ from scipy import stats
 def getFiles():
     #print("Location of files: ")
     #directory = input()
-    directory = '//home//sharon//Desktop//TCGA//BRCA//BRCA_allinone'
+    directory = '//home//sharon//Desktop//TCGA//BRCA//BRCA_allinone//edit'
 
     path = r"%s" % directory
     #dictionary for normal and tumor samples
@@ -28,9 +28,6 @@ def getFiles():
     howmanyN = dict()
     howmanyT = dict()
 
-    tempcount_tumor = 0
-    tempcount_normal = 0
-
     #01 at a specific location means tumor sample
     tpat = "01"
 
@@ -40,26 +37,18 @@ def getFiles():
 
     for file in os.listdir(path):
         #parse files for tumor samples
-        if fnmatch.fnmatch(file, '*.mirna.quantification.*') and (tpat==file[13:15]):
+        if fnmatch.fnmatch(file, '*.mirna.quantification.txt') and (tpat==file[13:15]):
             #print(file[13:15])
-            f = open('//home//sharon//Desktop//TCGA//BRCA//BRCA_allinone//%s' % file, 'r')
+            f = open('//home//sharon//Desktop//TCGA//BRCA//BRCA_allinone//edit//%s' % file, 'r')
             lines = f.readlines()
             #print(lines)
             for l in lines:
-                tempcount_tumor +=1
+
                 line = l.split('\t')
                 line= list(line)
                 #if it's the file title just ignore
                 if line[0].startswith("miRNA_ID"):
-                    tempcount_tumor -=1
                     continue
-                #if it's the first line of data, ignore the initial string
-                elif line[0].startswith("crossed-mapped"):
-                    temp = line[0][14:]
-                    allcountsT[temp]= float(line[2])
-                    howmanyT[temp] = 1
-                    allsamplesT.setdefault(temp,[])
-                    allsamplesT[temp].append(float(line[2]))
                 #otherwise, just add to dictionary and keep track of sample counts
                 elif line[0] in allcountsT:
                     allcountsT[line[0]]+= float(line[2])
@@ -73,28 +62,19 @@ def getFiles():
                     allsamplesT[line[0]].append(float(line[2]))
 
         #parse the files for normal samples
-        elif fnmatch.fnmatch(file, '*.mirna.quantification.*') and (tpat != file[13:15]):
+        elif fnmatch.fnmatch(file, '*.mirna.quantification.txt') and (tpat != file[13:15]):
 
             #f = open('C:\\Users\\Sharong\\Desktop\\Level_3\\%s' % file, 'r')
-            f = open('//home//sharon//Desktop//TCGA//BRCA//BRCA_allinone//%s' % file, 'r')
+            f = open('//home//sharon//Desktop//TCGA//BRCA//BRCA_allinone//edit//%s' % file, 'r')
 
             lines = f.readlines()
             #print(lines)
             for l in lines:
-                tempcount_normal +=1
                 line = l.split('\t')
                 line= list(line)
                 #if it's the file title just ignore
                 if line[0].startswith("miRNA_ID"):
-                    tempcount_normal -=1
                     continue
-                #if it's the first line of data, ignore the initial string
-                elif line[0].startswith("crossed-mapped"):
-                    temp = line[0][14:]
-                    allcountsN[temp]= float(line[2])
-                    howmanyN[temp] = 1
-                    allsamplesN.setdefault(temp,[])
-                    allsamplesN[temp].append(float(line[2]))
                 #otherwise, just add to dictionary and keep track of sample counts
                 elif line[0] in allcountsN:
                     allcountsN[line[0]]+= float(line[2])
@@ -108,10 +88,10 @@ def getFiles():
 
     #testing:
 
-    for k,v in allsamplesN.items():
-        if k=='hsa-mir-1322':
-            s = sum([float(x) for x in v])
-            print("sum", s)
+    #for k,v in allsamplesN.items():
+        #if k=='hsa-mir-1322':
+            #s = sum([float(x) for x in v])
+            #print("sum", s)
             #sum+= float(v)
 
 
@@ -132,18 +112,49 @@ def getFiles():
 
     #print "dict size: ", len(allcountsN), len(allsamplesT), len(allcountsT)
 
-    print("Tumor miRNA counts:")
-    print(allcountsT)
-    print("Normal miRNA counts:")
-    print(allcountsN)
+    #deleteDuplicates(allsamplesN,allsamplesT)
 
     return allcountsT,allcountsN,mirna_count, allsamplesN,allsamplesT
+
+"""
+#to make sure all samples have values for all miRNAs. to do  paired t-test
+def deleteDuplicates(allsamplesN, allsamplesT):
+    for k,v in allsamplesN.items():
+        if k not in allsamplesT:
+            print "DELETED"
+            del allsamplesN[k]
+        elif len(v) > len(allsamplesT[k]):
+            print "NOT EQUAL LENGTHS!!"
+    for k,v in allsamplesT.items():
+        if k not in allsamplesN:
+            print "DELETED"
+            del allsamplesT[k]
+"""
+
+
+# z = (expression in tumor sample) - (mean expression in normal sample)/ (standard deviation of expression in normal sample)
+def zScore(allsamplesN, allsamplesT):
+    import numpy
+    mean_dict = dict()
+    stdev_dict = dict()
+    for k,v in allsamplesN.items():
+        mean_dict[k] = numpy.mean(v)
+        stdev_dict[k] = numpy.std(v)
+
+    z_score = dict()
+    for k,v in allsamplesT.items():
+        z_score.setdefault(k,[])
+        for i in v:
+            z_score[k].append((i - mean_dict[k]) / stdev_dict[k])
+    print z_score
 
 
 
 def paired_tTest(total_mirna, allsamplesN, allsamplesT):
+
     n = total_mirna
     significant_count = 0
+    #checked - all miRNA collected in all samples
     #print("lengths: ", allsamplesT.__sizeof__(), allsamplesN.__sizeof__())
     for k,v in allsamplesT.items():
         #number of miRNAs apparently don't match so only use the ones that do
@@ -152,8 +163,6 @@ def paired_tTest(total_mirna, allsamplesN, allsamplesT):
             normal = allsamplesN[k]
             tumor = allsamplesT[k]
 
-            print "normal: ", len(normal)
-            print "tumor: ", len(tumor)
             paired_sample = stats.ttest_rel(normal,tumor)
 
 
@@ -170,6 +179,7 @@ def paired_tTest(total_mirna, allsamplesN, allsamplesT):
 def main():
     tumorDict, normalDict, total_mirna, allsamplesN, allsamplesT = getFiles()
     paired_tTest(total_mirna,allsamplesN,allsamplesT)
+    zScore(allsamplesN,allsamplesT)
 
 
 
