a9e91e7954beca21133cbf25a078c93eeb1fb672
sharg999 <sharg999@users.noreply.github.com>
2014-04-27 23:36:52 -0700

    Update mirnatarget.py

--- mirnatarget.py
+++ mirnatarget.py
@@ -2,6 +2,9 @@
 
 __author__ = 'sharon'
 
+#processes xls files
+import xlrd
+
 
 def microRNA():
     file = open("//home//sharon//Desktop//miRNA_targets//microrna.org//hg19_predictions_S_C_aug2010.txt", 'r')
@@ -32,10 +35,38 @@ def CLASH():
     return mirna_dict
 
 
+def MirTarBase():
+    file = xlrd.open_workbook("//home//sharon//Desktop//miRNA_targets//MirTarBase//hsa_MTI.xls")
+    worksheet = file.sheet_by_name('miRTarBase')
+
+    # key: miRNA , keys: [target gene name, target gene ID]
+    target_dict = dict()
+
+    for r in range(1,worksheet.nrows):
+        #if hit end of excel table
+        if worksheet.cell_type(r,1) in (xlrd.XL_CELL_EMPTY,xlrd.XL_CELL_BLANK):
+            break
+        else:
+            mirna = str(worksheet.cell(r,1).value)
+            gene_name = str(worksheet.cell(r,3).value)
+
+            #some miRNAs do not have target gene in the excel sheet. only get in the dict the good ones
+            gene_ID = worksheet.cell(r,4).value
+            if gene_ID != "":
+                #print gene_ID
+                gene_ID = int(round(float(worksheet.cell(r,4).value)))
+                target_dict[(mirna).lower()] = (gene_name.lower(), gene_ID)
+
+
+    #print target_dict
+    return target_dict
+
+
 def main():
 
-    microRNA()
-    CLASH()
+    #microRNA()
+    #CLASH()
+    MirTarBase()
 
 
 
